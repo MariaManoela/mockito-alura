4fecfae88173a5416210d51befb192bd21c5ea87
Maria Manoela <manoelamvc@hotmail.com>
2022-01-12 08:15:39 -0300

    Add BeforeEach annotation to instantiate mocks automatically

--- src/test/java/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/service/FinalizarLeilaoServiceTest.java
@@ -3,7 +3,9 @@ package service;
 import br.com.alura.leilao.dao.LeilaoDao;
 import br.com.alura.leilao.service.FinalizarLeilaoService;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class FinalizarLeilaoServiceTest {
 
@@ -12,8 +14,14 @@ public class FinalizarLeilaoServiceTest {
     @Mock
     private LeilaoDao leilaoDao;
 
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.service = new FinalizarLeilaoService(leilaoDao);
+    }
+
     @Test
     void deveriaFinalizarUmLeilao() {
-        service = new FinalizarLeilaoService(leilaoDao);
+
     }
 }
