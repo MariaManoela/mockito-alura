356530d82e349c78d348ae553e708c15402f2868
Maria Manoela <manoelamvc@hotmail.com>
2022-01-12 08:09:22 -0300

    Add mock annotation

--- src/test/java/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/service/FinalizarLeilaoServiceTest.java
@@ -1,14 +1,19 @@
 package service;
 
+import br.com.alura.leilao.dao.LeilaoDao;
 import br.com.alura.leilao.service.FinalizarLeilaoService;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class FinalizarLeilaoServiceTest {
 
     private FinalizarLeilaoService service;
 
+    @Mock
+    private LeilaoDao leilaoDao;
+
     @Test
     void deveriaFinalizarUmLeilao() {
-        service = new FinalizarLeilaoService();
+        service = new FinalizarLeilaoService(leilaoDao);
     }
 }
